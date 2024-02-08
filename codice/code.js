01524cb5a4ef206d1e566e91dc0890f34958c13d
salvatore_barbaro <salvatore.ilbarbaro97@gmail.com>
2024-02-08 15:09:44 +0100

    riproduzione esmpio in classe

--- codice/code.js
+++ codice/code.js
@@ -6,23 +6,89 @@
 // Potete realizzare lo stile che volete, sin quando tutto è leggibile con facilità :muscolo::carnagione-2:
 
 
-// primo passaggio da fare è che le celle si inseriscano automaticamente
+
+
+let number=prompt("inseerisci un numero");
+
 const grid = document.getElementById("grid");
 
+const arrayofnumber =generatearrayofnumber(number);
+
+
+// primo passaggio da fare è che le celle si inseriscano automaticamente
 // loop che ci permette di inserire i quadrati nella cella in modo automatico
-for(let i=0;i<49;i++)
+for(let i=0; i<number ;i++)
 {
     const newelement = document.createElement("div")
     newelement.classList.add("square");
     grid.append(newelement);
+    newelement.innerText=arrayofnumber[i];
     
 
-    // inserisco un event lit
+    // inserisco un event
     newelement.addEventListener("click",function()
     {
         // In JavaScript, la parola chiave this si riferisce a un oggetto.L'oggetto dipende dal modo in cui viene richiamato (utilizzato o chiamato).La parola chiave this si riferisce a oggetti diversi a seconda di come viene utilizzata:
-        this.classList.add("active");
-        // con la classe sopra riportata noi andiamo ad aggiungere la classe sctive all' elemento
+        this.classList.toggle("active");
+        // con la classe sopra riportata noi andiamo ad aggiungere la classe sctive all' elemento sctive e grazie a toggle noi possiamo metterle e toglierle
+        if(newelement.innerText %2 ==0)
+        {
+            // se è vero
+            this.classList.toggle("pari")
+        }
+        else
+        {
+            this.classList.toggle("dispari")
+        }
     }
+    
     );
+}
+
+
+
+
+
+// dobbiamo generare una funzione cheinserisca un numero solo se non è presente
+
+function generatearrayofnumber(number)
+{
+    // dichiarazione di un array
+    const arrayofnumber = [] ;
+
+    while(arrayofnumber.length < number)
+    {
+        const newelement= generateRandomNumber(number);
+        // if controlla se l'array non include il numero generato 
+        if(!arrayofnumber.includes(newelement))
+        {
+            arrayofnumber.push(newelement);
+        }
+    }
+
+    return arrayofnumber;
+    
+
+}
+
+
+// funzione per generare un numero randomico
+
+function generateRandomNumber(number)
+{
+    // dichiarazione variabile randomica,Math.floor per restituire numero intero,Math.random per valore randomico,maxnumber valore massimo , +1 perchè parte da 0
+    const randomnumber = Math.floor(Math.random()* number)+1 ;
+    // mi dara il valore numerico uscito dalla nostra funzione;
+    return randomnumber;
+}
+
+function pariodispari(number)
+{
+    if(number % 2 == 0)
+    {
+    }
+    else
+    {
+    }
+    
 }
