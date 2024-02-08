56a5752804104f13670f914a0b16471299a19072
salvatore_barbaro <salvatore.ilbarbaro97@gmail.com>
2024-02-08 19:35:01 +0100

    quasi fine , da sistemare un bug

--- codice/code.js
+++ codice/code.js
@@ -7,22 +7,46 @@
 
 
 
+const startbutton = document.getElementById("bottone");
+console.log(startbutton);
 
-let number=prompt("inseerisci un numero");
-
-const grid = document.getElementById("grid");
-
-const arrayofnumber =generatearrayofnumber(number);
+document.querySelector("#bottone").addEventListener("click",
+function()
+{
 
 
-// primo passaggio da fare è che le celle si inseriscano automaticamente
+    funzionamentogioco();
+
+}
+
+)
+
+function funzionamentogioco ()
+{
+    
+    const grid = document.getElementById("grid");
+
+    grid.innerHTML="";
+    // creazione elemento del dom
+    let newelement = document.createElement("div");
+
+selezione_difficolta(newelement);
+
+
+
+    // primo passaggio da fare è che le celle si inseriscano automaticamente
 // loop che ci permette di inserire i quadrati nella cella in modo automatico
 for(let i=0; i<number ;i++)
 {
-    const newelement = document.createElement("div")
+    // creazione elemento del dom
+    newelement = document.createElement("div");
+    // associazione class square al new element
     newelement.classList.add("square");
+    // appeso il nuovo elemento
     grid.append(newelement);
-    newelement.innerText=arrayofnumber[i];
+    // abbiamo inserito gli elementi dell'indice +1
+    newelement.innerText=[i+1];
+
     
 
     // inserisco un event
@@ -30,22 +54,41 @@ for(let i=0; i<number ;i++)
     {
         // In JavaScript, la parola chiave this si riferisce a un oggetto.L'oggetto dipende dal modo in cui viene richiamato (utilizzato o chiamato).La parola chiave this si riferisce a oggetti diversi a seconda di come viene utilizzata:
         this.classList.toggle("active");
+        console.log(this);
         // con la classe sopra riportata noi andiamo ad aggiungere la classe sctive all' elemento sctive e grazie a toggle noi possiamo metterle e toglierle
-        if(newelement.innerText %2 ==0)
-        {
-            // se è vero
-            this.classList.toggle("pari")
-        }
-        else
-        {
-            this.classList.toggle("dispari")
-        }
     }
     
     );
 }
+}
 
+// funzione per selezionare difficoltà
+function selezione_difficolta(newelement)
+    {
+       const difficult = document.getElementById("difficulty").value;
+    console.log(difficult);
 
+    if(difficult=="easy")
+    {
+        number=100;
+
+    //    grid.classList.add("easy");
+       grid.className=difficult;
+
+    }
+    else if(difficult=="medium")
+    {
+        number=81;
+        // grid.classList.add("medium");
+        grid.className=difficult;
+    }
+    else
+    {
+        number=49;
+        grid.innerHTML="";
+        grid.classList.add("difficult");
+    }
+    }
 
 
 
@@ -74,21 +117,21 @@ function generatearrayofnumber(number)
 
 // funzione per generare un numero randomico
 
-function generateRandomNumber(number)
-{
-    // dichiarazione variabile randomica,Math.floor per restituire numero intero,Math.random per valore randomico,maxnumber valore massimo , +1 perchè parte da 0
-    const randomnumber = Math.floor(Math.random()* number)+1 ;
-    // mi dara il valore numerico uscito dalla nostra funzione;
-    return randomnumber;
-}
+// function generateRandomNumber(number)
+// {
+//     // dichiarazione variabile randomica,Math.floor per restituire numero intero,Math.random per valore randomico,maxnumber valore massimo , +1 perchè parte da 0
+//     const randomnumber = Math.floor(Math.random()* number)+1 ;
+//     // mi dara il valore numerico uscito dalla nostra funzione;
+//     return randomnumber;
+// }
 
-function pariodispari(number)
-{
-    if(number % 2 == 0)
-    {
-    }
-    else
-    {
-    }
+// function pariodispari(number)
+// {
+//     if(number % 2 == 0)
+//     {
+//     }
+//     else
+//     {
+//     }
     
-}
+// }
